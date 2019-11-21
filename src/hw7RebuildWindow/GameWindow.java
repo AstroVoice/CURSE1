2055a03a28c5e743ad88dbb06dd25def6654f175
AstroVoice <alexander.strugovshchikov@gmail.com>
2019-11-21 12:00:32 +0300

    Start repaint window

--- src/hw7RebuildWindow/GameWindow.java
+++ src/hw7RebuildWindow/GameWindow.java
@@ -1,4 +1,14 @@
 package hw7RebuildWindow;
 
-public class GameWindow {
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+    private static final int WIN_WIDTH = 507;
+    private static final int WIN_HEIGHT = 555;
+    private static final int WIN_POSX = 650;
+    private static final int WIN_POSY = 250;
+    GameWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
